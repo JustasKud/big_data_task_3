98ec8129a91329542344b8f93c24ebfe564b21f5
Justas Kudirka <justas.kudirka@gmail.com>
2023-05-25 13:17:13 +0300

    Added task 2

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 
 ## Installation
 
+Requirements:
+
+- Docker installed.
+
+Start up docker:
+
 ```bash
 docker-compose up -d
 ```
 
+Initialize the config servers:
+
 ```bash
 docker-compose exec configsvr01 sh -c "mongosh < /scripts/init-configserver.js"
 ```
+
+Initialize the shards:
 
 ```bash
 docker-compose exec shard01-a sh -c "mongosh < /scripts/init-shard01.js"
@@ -22,21 +32,35 @@ docker-compose exec shard02-a sh -c "mongosh < /scripts/init-shard02.js"
 docker-compose exec shard03-a sh -c "mongosh < /scripts/init-shard03.js"
 ```
 
+Initialize the routers:
+
 ```bash
 docker-compose exec router01 sh -c "mongosh < /scripts/init-router.js"
 ```
+
+Access the primary router:
 
 ```bash
 docker-compose exec router01 mongosh --port 27017
 ```
 
+Enable sharding on a specified database:
+
 ```bash
-sh.enableSharding("MainDatabase")
+sh.enableSharding("shipDB")
+```
+
+Initialize ships and filtered_ships.
+
+```bash
+db.adminCommand( { shardCollection: "database.ships", key: { oemNumber: "hashed", zipCode: 1, supplierId: 1 } } )
 ```
 
 ```bash
-db.adminCommand( { shardCollection: "MainDatabase.MainCollection", key: { oemNumber: "hashed", zipCode: 1, supplierId: 1 } } )
+db.adminCommand( { shardCollection: "database.filtered_ships", key: { oemNumber: "hashed", zipCode: 1, supplierId: 1 } } )
 ```
+
+Exit the primary router:
 
 ```bash
 exit
@@ -49,12 +73,33 @@ Requirements:
 - virtualenv package
 - windows 10
 
+Initialize a virtual environment if not initialized:
+
 ```bash
 python -m virtualenv venv
 source ./venv/Scripts/activate
+```
+
+Install the requirements:
+
+```bash
 pip install -r requirements.txt
 ```
 
+To insert data, run this command:
+
 ```bash
-python main.py
+python main.py insert
+```
+
+To filter data, run this command:
+
+```bash
+python main.py filter
+```
+
+To perform calculations=, run this command:
+
+```bash
+python main.py calculate
 ```
--- main.py
+++ main.py
@@ -2,24 +2,87 @@ from pymongo import MongoClient
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
+import sys
+
+
+def insert_filtered_data(mmsi):
+	client = MongoClient("mongodb://127.0.0.1:27117,127.0.0.1:27118")
+	db = client.shipDB
+	
+	collection = db.ships
+	new_collection = db.filtered_ships
+	
+	pipeline = [
+		{"$match": {
+			"MMSI": mmsi["_id"], 
+			"Navigational status": {"$ne": float("nan")}, 
+			"Latitude": {"$ne": float("nan")}, 
+			"Longitude": {"$ne": float("nan")}, 
+			"ROT": {"$ne": float("nan")}, 
+			"SOG": {"$ne": float("nan")}, 
+			"COG": {"$ne": float("nan")}, 
+			"Heading": {"$ne": float("nan")},
+		}}
+	]
+	filtered_data = list(collection.aggregate(pipeline))
+	if len(filtered_data) >= 100:
+		new_collection.insert_many(filtered_data)
+	client.close()
+
 
 def insert_data(chunk):
-    client = MongoClient("mongodb://127.0.0.1:27117,127.0.0.1:27118")
-    db = client.MainDatabase
-    collection = db.MainCollection
-    collection.insert_many(chunk.to_dict('records'))
-    client.close()
+	client = MongoClient("mongodb://127.0.0.1:27117,127.0.0.1:27118")
+	db = client.shipDB
+	collection = db.ships
+	collection.insert_many(chunk.to_dict('records'))
+	client.close()
+
+
+def task_1():
+	PATH = "./data/aisdk-2023-05-01.csv"
+	CHUNK_SIZE = 1000
+	MAX_ROWS = 100_000
+	MAX_WORKERS = 16
+	
+	data = list(pd.read_csv(PATH, chunksize=CHUNK_SIZE, nrows=MAX_ROWS))
+	
+	with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+		futures = {executor.submit(insert_data, chunk): chunk for chunk in data}
+		for future in tqdm(as_completed(futures), total=len(data)):
+			pass
+
+
+def task_2():
+	MAX_WORKERS = 16
+	client = MongoClient("mongodb://127.0.0.1:27117,127.0.0.1:27118")
+	db = client.shipDB
+	collection = db.ships
+	collection.create_index([("MMSI", 1)])
+	
+	count_pipeline = [
+		{"$group": {"_id": "$MMSI", "count": {"$sum": 1}}},
+		{"$match": {
+			"count": {"$gt": 100},
+		}}
+	]
+	
+	over_100_data = list(collection.aggregate(count_pipeline))
+	client.close()
+	
+	with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+		futures = {executor.submit(insert_filtered_data, chunk): chunk for chunk in over_100_data}
+		for future in tqdm(as_completed(futures), total=len(over_100_data)):
+			pass
 
 
 if __name__ == "__main__":
-    PATH = "./data/aisdk-2023-05-01.csv"
-    CHUNK_SIZE = 1000
-    MAX_ROWS = 300_000
-    MAX_WORKERS = 16
-    
-    data = list(pd.read_csv(PATH, chunksize=CHUNK_SIZE, nrows=MAX_ROWS))
-    
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        futures = {executor.submit(insert_data, chunk): chunk for chunk in data}
-        for future in tqdm(as_completed(futures), total=len(data)):
-            pass
+	if len(sys.argv) < 2:
+		print("You need to add an argument 'insert','filter', or 'calculate'.")
+	elif sys.argv[1] == "insert":
+		task_1()
+	elif sys.argv[1] == "filter":
+		task_2()
+	elif sys.argv[1] == "calculate":
+		task_3()
+	else:
+		print("Incorrect argument. You need to use 'insert','filter', or 'calculate'.")
